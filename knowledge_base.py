d80203459dfd49e983a5ca996dfbc3d34b53c675
Luke Marden <fxg18asu@uea.ac.uk>
2021-01-06 17:40:29 +0000

    Luke - Rules done

--- knowledge_base.py
+++ knowledge_base.py
@@ -6,7 +6,7 @@ class Ticket(Fact):
     def __init__(self):
         self.name = None
         self.isReturn = None
-        self.startLocation = None
+        self.origin = None
         self.destination = None
         self.departTime = None
         self.departDate = None
@@ -42,23 +42,142 @@ class TrainBooking(KnowledgeEngine):
             self.declare(Fact(name = self.ticket.name))
             self.knowledge['name'] = self.ticket.name
         else:
-            if self.knowledge['question'] == 'ask_name':
+            if self.knowledge['question'] == 'askName':
                 print("AskName")
                 # Message.emit_feedback('display received message', 'unknown_message')
             else:
-                self.knowledge['question'] = 'ask_name'
+                self.knowledge['question'] = 'askName'
                 # Message.emit_feedback('display received message', 'ask_name')
 
     @Rule(Fact(action='greet'), Fact(name = MATCH.name))
     def service(self):
         print("TEST")
-        if self.knowledge['question'] == 'ask_service':
+        if self.knowledge['question'] == 'askService':
             print("AskService1")
             # Message.emit_feedback('display received message', 'unknown_message')
         else:
-            self.knowledge['question'] = 'ask_service'
+            self.knowledge['question'] = 'askService'
             print("AskService2")
             # Message.emit_feedback('display received message', 'ask_booking')
+
+    @Rule(Fact(action='book'), NOT(Fact(destination=W())), NOT(Fact(origin=W())))
+    def destination(self):
+        if self.ticket.destination is not None:
+            self.declare(Fact(destination = self.ticket.destination))
+            self.knowledge['destination'] = self.ticket.destination
+        else:
+            if self.knowledge['question'] == 'destination':
+                print()
+            else:
+                self.knowledge['question'] = 'destination'
+                print()
+
+    @Rule(Fact(action='book'), Fact(destination=MATCH.destination), NOT(Fact(origin=W())))
+    def origin(self):
+        if self.ticket.origin is not None:
+            self.declare(Fact(destination = self.ticket.origin))
+            self.knowledge['destination'] = self.ticket.origin
+        else:
+            if self.knowledge['question'] == 'origin':
+                print()
+            else:
+                self.knowledge['question'] = 'origin'
+                print()
+
+    @Rule(Fact(action='book'), Fact(destination=MATCH.destination),
+          Fact(origin=MATCH.origin), NOT(Fact(isReturn=W())))
+    def isReturn(self):
+        if self.ticket.destination is not None:
+            self.declare(Fact(isReturn = self.ticket.isReturn))
+            self.knowledge['return'] = self.ticket.isReturn
+        else:
+            if self.knowledge['question'] == 'return':
+                print()
+            else:
+                self.knowledge['question'] = 'return'
+                print()
+
+    @Rule(Fact(action='book'), Fact(destination=MATCH.destination),
+          Fact(origin=MATCH.origin), Fact(isReturn=MATCH.isReturn),
+          NOT(Fact(departDate=W())), NOT(Fact(departTime=W())))
+    def departDate(self):
+        if self.ticket.departDate is not None:
+            self.declare(Fact(departDate=self.ticket.departDate))
+            self.knowledge['departDate'] = self.ticket.departDate
+        else:
+            if self.knowledge['question'] == 'departDate':
+                print()
+            else:
+                self.knowledge['question'] = 'departDate'
+                print()
+
+    @Rule(Fact(action='book'), Fact(destination=MATCH.destination),
+          Fact(origin=MATCH.origin), Fact(isReturn=MATCH.isReturn),
+          Fact(departDate=MATCH.departDate), NOT(Fact(departTime=W())))
+    def departTime(self):
+        if self.ticket.departTime is not None:
+            self.declare(Fact(departTime=self.ticket.departTime))
+            self.knowledge['departTime'] = self.ticket.departTime
+        else:
+            if self.knowledge['question'] == 'departTime':
+                print()
+            else:
+                self.knowledge['question'] = 'departTime'
+                print()
+
+    @Rule(Fact(action='book'), Fact(destination=MATCH.destination),
+          Fact(origin=MATCH.origin), Fact(isReturn=True),
+          Fact(departDate=MATCH.departDate), Fact(departTime=MATCH.departTime),
+          NOT(Fact(returnDate=W())), NOT(Fact(returnTime=W())))
+    def returnDate(self):
+        if self.ticket.returnDate is not None:
+            self.declare(Fact(returnDate=self.ticket.returnDate))
+            self.knowledge['returnDate'] = self.ticket.returnDate
+        else:
+            if self.knowledge['question'] == 'returnDate':
+                print()
+            else:
+                self.knowledge['question'] = 'returnDate'
+                print()
+
+    @Rule(Fact(action='book'), Fact(destination=MATCH.destination),
+          Fact(origin=MATCH.origin), Fact(isReturn=True),
+          Fact(departDate=MATCH.departDate), Fact(departTime=MATCH.departTime),
+          Fact(returnDate=MATCH.returnDate), NOT(Fact(returnTime=W())))
+    def returnTime(self):
+        if self.ticket.returnTime is not None:
+            self.declare(Fact(returnTime=self.ticket.returnTime))
+            self.knowledge['returnTime'] = self.ticket.returnTime
+        else:
+            if self.knowledge['question'] == 'returnTime':
+                print()
+            else:
+                self.knowledge['question'] = 'returnTime'
+                print()
+
+    @Rule(Fact(action='book'), Fact(destination=MATCH.destination),
+          Fact(origin=MATCH.origin), Fact(isReturn=False),
+          Fact(departDate=MATCH.departDate), Fact(departTime=MATCH.departTime),
+    def singleTicket(self):
+        if self.knowledge['question'] == 'singleTicket':
+            print()
+        else:
+            self.knowledge['question'] = 'singleTicket'
+            print()
+
+    @Rule(Fact(action='book'), Fact(destination=MATCH.destination),
+          Fact(origin=MATCH.origin), Fact(isReturn=True),
+          Fact(departDate=MATCH.departDate), Fact(departTime=MATCH.departTime),
+          Fact(returnDate=MATCH.returnDate), Fact(returnTime=MATCH.returnTime))
+    def returnTicket(self):
+        if self.knowledge['question'] == 'returnTicket':
+            print()
+        else:
+            self.knowledge['question'] = 'returnTicket'
+            print()
+
+
+
 
 
 if __name__ == '__main__':
