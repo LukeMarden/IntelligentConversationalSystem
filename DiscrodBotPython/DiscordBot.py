ba5e37c92e4da80071e2fca266f6f77aa7bd39a6
svene <pzk18uxu@uea.ac.uk>
2021-01-07 15:48:21 +0000

    Discord added -sven

---
+++ DiscrodBotPython/DiscordBot.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = '.')
+
+@client.event
+async def on_ready():
+    print('Train bot is ready!')
+
+@client.event
+async def on_member_join(member):
+    print(f'Welcome (member)! How can I help?')
+
+@client.event
+async def on_member_remove(member):
+    print(f'(member) sad to see you go. Bye-bye!')
+
+@client.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+client.run('Nzk2NzQxOTg0NDQ1NzkyMjc2.X_cVyw.-2yYSLY34sKpL3keAFzEjwAWgdU')
