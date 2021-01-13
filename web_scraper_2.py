cf5b49f83f03e2864faaa8a0e54f6da9b250859c
Jacy <jacy2000@hotmail.co.uk>
2021-01-13 15:34:55 +0000

    Jacy S

--- web_scraper_2.py
+++ web_scraper_2.py
@@ -1,5 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+
+from web_scraper import driver
+
 URL = 'https://ojp.nationalrail.co.uk/service/timesandfares/IPS/NRW/today/1545/dep'
 page = requests.get(URL)
 
@@ -7,6 +11,8 @@ soup = BeautifulSoup(page.content, 'html.parser')
 
 results = soup.find(id='oft')
 trains = results.find_all(class_='mtx')
+
+Trains_list = []
 # print(trains[0])
 # for train in trains:
 #     print(train, end='\n'*2)
@@ -14,7 +20,20 @@ for train in trains:
     depart = train.find(class_='dep').text.strip()
     origin = train.find(class_='result-station').text.strip()
     duration = train.find(class_='dur').text.strip()
-    print(depart)
-    print(origin)
-    print(duration)
-    print()
+    arrive = train.find(class_='arr').text.strip()
+    Destination = train.find(class_='result-station').text.strip()
+    Faire = train.find(class_='opsingle').text.strip()
+    Train_item = {
+        'depart': depart,
+        'origin': origin,
+        'duration': duration,
+        'arrive': arrive,
+        'Destination': Destination,
+        'Faire': Faire
+    }
+
+    Trains_list.append(Train_item)
+
+df = pd.DataFrame(Trains_list)
+print(df)
+driver.quit()
