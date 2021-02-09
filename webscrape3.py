561b971fb5aea7d9a91b59b1b73eb98dcb09b56a
lukemarden <lmarden7@gmail.com>
2021-02-09 02:39:29 +0000

    Luke - Comments

--- webscrape3.py
+++ webscrape3.py
@@ -1,11 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
-
+# ************************************************************************************************************
+#
+#
+#    webscrape3.py
+# This class is used to webscrape the cheapest ticket
+#
+#
+#    By: Group 18
+#
+#    Created: 03/01/2021
+#
+# ************************************************************************************************************
 class CheapestTicket():
+
+    # This method is used to initialise the classes variables
+    # url: The url to scrape from
+    # isReturn: whether the ticket is a return or not
     def __init__(self, url, isReturn):
         self.url = url
         self.isReturn = isReturn
 
+    # This method finds the cheapest ticket
+    # return: The cheapest ticket price
     def find_cheapest_ticket(self):
         page = requests.get(self.url)
         soup = BeautifulSoup(page.content, 'html.parser')
