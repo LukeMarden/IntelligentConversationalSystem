bb491a99042d3e063d3dc9cf62c34bdd375b3ee5
Sven <sven.elbert5@gmail.com>
2021-02-09 13:08:16 +0000

    Sven

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from knowledge_base import *
 from nlpspacy import extract_info
 from prediction import *
+
 # from DiscrodBotPython.DiscordBot import *
 # from web_scraper import *
 # from nlpuspacy import *
@@ -25,23 +26,27 @@ if __name__ == '__main__':
     # print(ticket.find_cheapest())
     # pred = prediction('Norwich', 'London Liverpool Street', 9, 'Diss', 10, 1200) #check that it contains all these stations
 
-
     question = {}
     kb = TrainBooking()
-    kb.knowledge = {'service': 'predict'}
+    kb.knowledge = {'service': 'book'}
     kb.reset()
     kb.run()
+    #
+    # kb.knowledge['destination'] = 'Ipswich'
+    # kb.knowledge['origin'] = 'Diss'
+    # kb.knowledge['delayTime'] = 5
+    # kb.knowledge['numberOfStops'] = 8
+    # kb.knowledge['delayStation'] = 'Norwich'
+    # kb.knowledge['delayCode'] = 10
+    # kb.knowledge['arrivalTime'] = 1200
+    # kb.reset()
+    # kb.run()
+    # print(kb.knowledge['response'])
 
-    kb.knowledge['destination'] = 'Ipswich'
-    kb.knowledge['origin'] = 'Diss'
-    kb.knowledge['delayTime'] = 5
-    kb.knowledge['numberOfStops'] = 8
-    kb.knowledge['delayStation'] = 'Norwich'
-    kb.knowledge['delayCode'] = 10
-    kb.knowledge['arrivalTime'] = 1200
-    kb.reset()
-    kb.run()
-    print(kb.knowledge['response'])
+    message = "12:00"
+    question['question'] = 'arrivalTime'
+    info = extract_info(question, message, kb)
+    print(info)
 
     # #Discord
     # client.run('Nzk2NzQxOTg0NDQ1NzkyMjc2.X_cVyw.-2yYSLY34sKpL3keAFzEjwAWgdU')
--- nlpspacy.py
+++ nlpspacy.py
@@ -1,108 +1,135 @@
 import json
 import re
-
 import dateutil
 import spacy
-from spacy.lang.en import English as english
+from spacy.lang.en import English
 from knowledge_base import *
 
-parser = english()
+# ************************************************************************************************************
+#
+#    nlpu.py
+#    This file contains the main contents of the NLP as well as a function extract_info()
+#    to extract all the contents of message given by a user
+#
+#    Author: Group 18 (London is Blue)
+#
+#    Created: 03/01/2021
+#
+# ************************************************************************************************************
+
+# Loading in and defining the main modules
 nlp = spacy.load("en_core_web_lg")
+parser = English()
+
+# Contains all the locations a user can input
+stations = json.load(open('train_codes.json'))
+
+# Defining regex patterns used for rule-based matching
+true = r'.*(yes|yeah|yh|correct|true|).*'
+false = r'.*(no|nah|nay|wrong|false).*'
+time = r'^(([01]\d|2[0-3]):([0-5]\d)|24:00)$'
+default_time = r'^([0-1][0-9]|[2][0-3]):([0-5][0-9])$'
+time_am = r'/((1[0-2]|0?[1-9]):([0-5][0-9]) ?([AaPp][Mm]))/'
 
 
-# for token in doc:
-#     print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
-#           token.shape_, token.is_alpha, token.is_stop)
-
+# This method takes in three parameters: question, message and kb
+# used to extract the information from a users message and returns a dictionary results
+#
+# question: a dictionary, which defines at what stage the question is at
+# message: a string, which is input by the user
+# kb: a class, knowledge base handles the engine and what questions will be required
 def extract_info(question, message, kb):
+    # spaCy's initiation of the nlp
     doc = nlp(message)
-    stations = json.load(open('train_codes.json'))
+    # initialises a dict containing the results of the function
     results = {}
-    # print(doc)
-    # true = re.compile(r'\b(?i)(true|yes|yeah|yh|correct)\b')
-    true = r'.*(yes|correct|true|yeah|yh).*'
-    false = r'.*(no|wrong|false|nah|nay).*'
-    # time = r'^([0-1][0-9]|[2][0-3]):([0-5][0-9])$'
-    time = re.compile(r'^(([01]\d|2[0-3]):([0-5]\d)|24:00)$')
-    timeAM = r'/((1[0-2]|0?[1-9]):([0-5][0-9]) ?([AaPp][Mm]))/'
 
-    # Extract return
+    # Testing to see if the message gets decoded and all the functions get recognized
+    for token in doc:
+        print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
+              token.shape_, token.is_alpha, token.is_stop)
+
+    # Runs through all entities in message to extract if its a return or a single ticket
     for ent in doc:
         ent = str(ent).lower()
-        x = re.search(true, ent)
-        y = re.search(false, ent)
+        search_true = re.search(true, ent)
+        search_false = re.search(false, ent)
         if ent in {'return', 'returns'}:
             results['return'] = True
         if 'come back' in str(doc):
             results['return'] = True
         if 'go back' in str(doc):
             results['return'] = True
-        if x:
+        if search_true:
             results['return'] = True
         if ent in {'single', 'singles'}:
             results['return'] = False
-        if y:
+        if search_false:
             results['return'] = False
 
-    # Extract locations
+    # Runs through all entities in message and then compares     them to all the available
+    # stations in the json file and extracts the location to the results
     locations = []
     for ent in doc:
-        # if ent.label_ == 'GPE':
-        #     locations.append(str(ent[0]))
-        #     results['location'] = locations
-        # else:
-            for i in stations:
-                # print(ent.text)
-                if ent.text == i:
-                    locations.append(i)
-                    results['location'] = locations
+        for i in stations:
+            if ent.text == i:
+                locations.append(i)
+                results['location'] = locations
 
-    # minutes = []
-    # times = []
-    # for ent in doc.ents:
-    #     print(ent.label_)
-    #     if ent.text.isdigit():
-    #         minutes.append(ent.text)
-    #
-    # if len(minutes) > 0:
-    #     results['minutes'] = minutes
-
+    # Runs through all entities in the message to extract either the integer, time or date of an entity
+    # after which extracted to the results
+    integers = []
     minutes = []
     dates = []
     times = []
-    integers = []
+    search_time = re.search(time, doc.text)
+
+    # For 0 digits
     for ent in doc:
         if ent.text == '0':
             integers.append(ent.text)
+    # For all cardinal numbers
     for ent in doc.ents:
         if ent.label_ == 'CARDINAL':
             integers.append(ent.text)
             continue
-        # Minutes
+        # For minutes
         if ent.text.isdigit():
             minutes.append(ent.text)
 
-        # Dates
+        # For any dates
         if ent.label_ == 'DATE':
             date = dateutil.parser.parse(ent.text)
             date = str(date.day).zfill(2) + str(date.month).zfill(2) + (str(date.year)[2:])
             dates.append(date)
 
-        # Times
+        # For any times
         if ent.label_ == 'TIME':
             date = dateutil.parser.parse(ent.text)
             times.append(str(date.hour).zfill(2) + str(date.minute).zfill(2))
 
-    if time.search(str(message)):
-        date = dateutil.parser.parse(str(message))
+    # Uses the regex and runs through the message to search for time format
+    if search_time:
+        date = dateutil.parser.parse(doc.text)
         times.append(str(date.hour).zfill(2) + str(date.minute).zfill(2))
 
+    results['integers'] = integers
     results['minutes'] = minutes
     results['dates'] = dates
     results['times'] = times
 
-    results['integers'] = integers
+    # Prints out all the results that have been extracted
+    print(results)
 
+    # Contains the logic to apply the method to get result to the right question
+    # Also contains basic error handling, since if results != 1
+
+    # Results logic
+    if (question['question'] == 'return') and ('return' in results):
+        kb.knowledge['return'] = results['return']
+        results = 1
+
+    # Location logic
     if (question['question'] == 'origin') and ('location' in results):
         kb.knowledge['origin'] = results['location'][0]
         results = 1
@@ -113,10 +140,7 @@ def extract_info(question, message, kb):
         kb.knowledge['delayStation'] = results['location'][0]
         results = 1
 
-    if (question['question'] == 'return') and ('return' in results):
-        kb.knowledge['return'] = results['return']
-        results = 1
-
+    # Date logic
     if (question['question'] == 'departDate') and ('dates' in results):
         kb.knowledge['departDate'] = results['dates'][0]
         results = 1
@@ -133,6 +157,7 @@ def extract_info(question, message, kb):
         kb.knowledge['arrivalTime'] = results['times'][0]
         results = 1
 
+    # Number logic
     if (question['question'] == 'numberOfStops') and ('integers' in results):
         kb.knowledge['numberOfStops'] = results['integers'][0]
         results = 1
@@ -143,6 +168,4 @@ def extract_info(question, message, kb):
         kb.knowledge['delayCode'] = results['integers'][0]
         results = 1
 
-    print(results)
     return results
-
